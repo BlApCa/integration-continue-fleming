7a26bb50fc0123cd61647503d8e8ccee732b042e
BlApCa <fleming.duffard@gmail.com>
2025-04-08 12:24:46 +0200

    intégration front #5

--- src/App.js
+++ src/App.js
@@ -13,7 +13,25 @@ import './App.css';
 function App() {
   const [successful, setSuccessful] = useState(false);
 
+    const MyScreen = document.getElementById('S')
+    const MiniScreen = document.getElementById('MiniScreen')
+    // let ActualTime = document.getElementById('ActualTime')
+    // let ActualDate = document.getElementById('ActualDate')
+    let currentHours = new Date().getHours()
+    let currentMinutes = new Date().getMinutes()
+    currentMinutes < 10 ? currentMinutes = '0'+currentMinutes : null
+    let currentTime = `${currentHours}:${currentMinutes}`
+    console.log(currentTime)
+    let currentDate = new Date().toLocaleDateString('fr-FR')
+    let opened = true;
 
+    const pokedexOpener = (size,transition,opacity) =>
+        (MyScreen.style.width = size,MiniScreen.style.transition = transition,MiniScreen.style.opacity = opacity, opened = !opened)
+
+    const test = () =>
+        opened ?
+            (pokedexOpener('30vw','opacity 0.2s 0.4s','1'),ActualTime.append(currentTime), ActualDate.append(currentDate))
+            : pokedexOpener('90vw','opacity 0.1s','0')
 
 
 
@@ -36,8 +54,8 @@ function App() {
                 </div>
                 <div id="MiniScreen">
                     <div className="ms-filter"></div>
-                    <h1 id="ActualTime"></h1>
-                    <h3 id="ActualDate"></h3>
+                    {/*<h1 id="ActualTime"></h1>*/}
+                    {/*<h3 id="ActualDate"></h3>*/}
                 </div>
                 <div id="R-H" className="handle">
                     <div className="r_filter filter"></div>
