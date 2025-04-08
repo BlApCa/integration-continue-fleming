1c9863d33172c29f7d0902d13f6a1960d723e63c
BlApCa <fleming.duffard@gmail.com>
2025-04-08 12:18:17 +0200

    intégration front #5

--- src/App.js
+++ src/App.js
@@ -34,6 +34,11 @@ function App() {
                     </div>
                     <div id="Clicker-Btn" className="mybutton"></div>
                 </div>
+                <div id="MiniScreen">
+                    <div className="ms-filter"></div>
+                    <h1 id="ActualTime"></h1>
+                    <h3 id="ActualDate"></h3>
+                </div>
                 <div id="R-H" className="handle">
                     <div className="r_filter filter"></div>
                     <div id="R-C" className="circle">
